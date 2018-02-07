9bcbe12a402715b3f1a03519c5a00679e335946a
kissge <mail@yo.eki.do>
2018-02-07 08:58:58 +0000

    Hitting Enter with empty input invokes git status in zsh

---
+++ common/05-hook.zsh
@@ -0,0 +1,12 @@
+# original source: https://superuser.com/a/625663
+function magic-enter() {
+    if [ -z "$BUFFER" ]; then
+        echo
+        git status
+        zle reset-prompt
+    else
+        zle accept-line
+    fi
+}
+zle -N magic-enter
+bindkey "^M" magic-enter

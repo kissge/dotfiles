d3726419b1f2136c8f63215b05c29fe5da63b582
Yusuke Kido <mail@yusuk.eki.do>
2022-03-19 21:24:06 +0900

    Force show progress

--- common/03-builtin.sh
+++ common/03-builtin.sh
@@ -56,7 +56,7 @@ if [ -t 0 ]; then # stdin is tty
         if [ "$1" = clone ]; then
             # git clone and automatically cd
             {
-                builtin cd "$(command git -c color.ui=always "$@" 3>&1 1>&2 2>&3 3>&- | tee /dev/fd/3 | awk -F\' '/Cloning into/ {print $2}')"
+                builtin cd "$(command git -c color.ui=always "$@" --progress 3>&1 1>&2 2>&3 3>&- | tee /dev/fd/3 | awk -F\' '/Cloning into/ {print $2}')"
             } 3>&2 2>&1
         else
             command git "$@"

7797d2fde1975d1b047403dfcea582abe6e0088e
Yusuke Kido <mail@yusuk.eki.do>
2022-03-22 16:09:14 +0900

    Use Alt + ←/→

--- common/01-environment.zsh
+++ common/01-environment.zsh
@@ -1,6 +1,9 @@
 # shellcheck shell=bash
 
 bindkey "^U" backward-kill-line
+bindkey "[C" forward-word
+bindkey "[D" backward-word
+
 setopt NO_NOMATCH
 # shellcheck disable=SC2034
 ZLE_REMOVE_SUFFIX_CHARS=$' \t\n;&'

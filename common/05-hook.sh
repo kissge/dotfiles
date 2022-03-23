bc06c5cc54cc0ee61667cccf86fbf8d09a21e5ea
Yusuke Kido <mail@yusuk.eki.do>
2022-03-23 23:24:50 +0900

    Downloadsフックをちょっと改良
    1. コメントが逆になっていたのを修正
    2. 色がつく
    3. フルパスで表示しない
    4. ls -h オプションを追加
    5. 不要なローカル変数は消すようにし、無理なものは変数名をprivateっぽくする（localは厳密にはPOSIXにはないため使わない）

--- common/05-hook.sh
+++ common/05-hook.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-DOWNLOADS=${HOME}/Downloads/
+_DOWNLOADS="${HOME}/Downloads"
 
-if [ ! "$EMACS" ] && [ -d "$DOWNLOADS" ]; then
+if [ ! "$EMACS" ] && [ -d "$_DOWNLOADS" ]; then
     # shellcheck disable=SC2185
     if find -atime +1d 2>/dev/null; then
-        # BSD
-        FINDCOMMAND="find $DOWNLOADS -maxdepth 1 -mindepth 1 -atime +3d"
-    else
         # GNU
-        FINDCOMMAND="find $DOWNLOADS -maxdepth 1 -mindepth 1 -atime +3"
+        _FIND_COMMAND='find * .* -maxdepth 0 -atime +3d'
+    else
+        # BSD
+        _FIND_COMMAND='find * .* -maxdepth 0 -atime 3'
     fi
 
-    OLDERFILES=$(eval "$FINDCOMMAND" -exec ls -tdlu {} \+)
+    if command ls --color >/dev/null 2>&1; then
+        # GNU
+        _LS_COMMAND='ls --color=always'
+    else
+        # BSD
+        _LS_COMMAND='env CLICOLOR=1 CLICOLOR_FORCE=1 ls'
+    fi
+
+    OLDERFILES=$(
+        cd "$_DOWNLOADS"
+        eval "$_FIND_COMMAND" -exec "$_LS_COMMAND" -tdluh {} \+
+    )
 
     if [ -n "$OLDERFILES" ]; then
         echo 'These file(s) have not been accessed for more than three days:' >&2
@@ -20,8 +31,11 @@ if [ ! "$EMACS" ] && [ -d "$DOWNLOADS" ]; then
         echo 'To delete these files, type `clean_older_files'"'"'.' >&2
     fi
 
+    unset OLDERFILES
+
     clean_older_files() {
-        eval "$FINDCOMMAND" -exec rm -rfv {} \+ | command less -RFX
+        cd "$_DOWNLOADS"
+        eval "$_FIND_COMMAND" -exec rm -rfv {} \+ | command less -RFX
     }
 fi
 

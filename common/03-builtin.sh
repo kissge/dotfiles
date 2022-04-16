423d18d5cd66909dfd6375fca4ab20e3dc39b233
Yusuke Kido <mail@yusuk.eki.do>
2022-04-16 23:35:06 +0900

    export-dotenv: bash 3/4 対応

--- common/03-builtin.sh
+++ common/03-builtin.sh
@@ -22,20 +22,22 @@ function cd() {
 }
 
 function export-dotenv() {
-    local dotenv=${1:-.env}
+    local dotenv=${1:-./.env}
 
     if [ ! -f "$dotenv" ]; then
         echo "No such file: $dotenv"
         return 1
     fi
 
-    local temp1=$(mktemp)
-    local temp2=$(mktemp)
-
-    env -i bash --noprofile --norc -c 'declare -p' >"${temp1}"
-    env -i bash --noprofile --norc -c '. "'"${dotenv}"'" && declare -p' >"${temp2}"
-    exports=$(diff --old-line-format="" --new-line-format="export %L" --unchanged-line-format="" "${temp1}" "${temp2}" |
-        grep -v '^export \(BASH_EXECUTION_STRING\|PIPESTATUS\|_\)=')
+    exports=$(diff --old-line-format="" --new-line-format="%L" --unchanged-line-format="" \
+        <(env -i bash --noprofile --norc -c 'declare -p') \
+        <(env -i bash --noprofile --norc -c '. "'"${dotenv}"'" && declare -p') |
+        if "${SHELL:-$0}" --version | grep -qF 'GNU bash, version 3'; then
+            sed -e 's/^/export /'
+        else
+            sed -E 's/^(declare( -- ?)?)?/declare -g -x /'
+        fi |
+        grep -v ' \(BASH_ARGC\|BASH_EXECUTION_STRING\|PIPESTATUS\|_\|PPID\)=')
 
     eval "${exports}"
 

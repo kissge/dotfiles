16e87679f301cfe847f909645f8c37cdca90cb98
Yusuke Kido <mail@yusuk.eki.do>
2022-06-09 20:16:01 +0900

    Update ignored linter rule

--- lint.sh
+++ lint.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck disable=SC2046
-shellcheck --exclude=SC1090,SC1091,SC2155,SC2164 "$@" \
+shellcheck --exclude=SC1090,SC1091,SC2016,SC2155,SC2164 "$@" \
     init \
     lint.sh \
     common/??-* \
@@ -12,5 +12,6 @@ shellcheck --exclude=SC1090,SC1091,SC2155,SC2164 "$@" \
 
 # SC1090 Can't follow non-constant source. Use a directive to specify location.
 # SC1091 Not following: (error message here)
+# SC2016 Expressions don't expand in single quotes, use double quotes for that.
 # SC2155 Declare and assign separately to avoid masking return values.
 # SC2164 Use cd ... || exit in case cd fails.

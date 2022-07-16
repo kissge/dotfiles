ba3d63a0d58bdfbfcdaddebf1d70a7fe628a830b
Yusuke Kido <mail@yusuk.eki.do>
2022-07-16 20:59:43 +0900

    Improvement on cd: find git root, perfectly

--- zsh/inits/20-builtin.zsh
+++ zsh/inits/20-builtin.zsh
@@ -5,22 +5,25 @@ function md() {
 function cd() {
     if [ $# -eq 0 ]; then
         local git_root
-        if git_root=$(git rev-parse --show-toplevel 2>/dev/null); then
-            if [ "$git_root" != "$PWD" ]; then
-                builtin cd "$git_root"
-                return
-            fi
+
+        if ! git_root=$(git rev-parse --show-toplevel 2>/dev/null); then
+            # Not inside a git repo, so fallback to ~/
+            builtin cd
+        elif [ "$git_root" != "$PWD" ] || git_root=$(cd .. && git rev-parse --show-toplevel 2>/dev/null); then
+            # Found git root
+            builtin cd "$git_root"
+        else
+            # Now at git root, which is the only one under ~/ or /, so fallback to ~/
+            builtin cd
         fi
-
-        builtin cd
-        return
-    fi
-
-    local last_arg="${@:$#}"
-
-    if [ -f "$last_arg" ]; then
-        builtin cd "${@:1:$(($# - 1))}" "$(dirname -- "$last_arg")"
     else
-        builtin cd "$@"
+        local last_arg="${@:$#}"
+
+        if [ -f "$last_arg" ]; then
+            # File is given, cd to its directory
+            builtin cd "${@:1:$(($# - 1))}" "$(dirname -- "$last_arg")"
+        else
+            builtin cd "$@"
+        fi
     fi
 }

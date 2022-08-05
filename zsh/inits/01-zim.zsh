0d03c51104a6181b35976b601644ff3b40c1006b
Yusuke Kido <mail@yusuk.eki.do>
2022-08-05 18:39:22 +0900

    batない環境対応

--- zsh/inits/01-zim.zsh
+++ zsh/inits/01-zim.zsh
@@ -17,7 +17,7 @@ if [[ ! "${ZIM_HOME}"/init.zsh -nt "${ZDOTDIR:-${HOME}}"/.zimrc ]]; then
   source "${ZIM_HOME}"/zimfw.zsh init -q
 fi
 
-FZF_PREVIEW='sed -e "s/^ *[0-9]*  //; s/\\\\n/\n/g" <<< {} | bat --plain --color=always --language=zsh'
+FZF_PREVIEW='sed -e "s/^ *[0-9]*  //; s/\\\\n/\n/g" <<< {} | (bat --plain --color=always --language=zsh || cat) 2>/dev/null'
 FZF_PREVIEW_WINDOW=':wrap'
 
 # Initialize modules.

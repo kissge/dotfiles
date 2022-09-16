6ed0b227ca52c30627781e05b6030c1e90a051b1
Yusuke Kido <mail@yusuk.eki.do>
2022-09-16 14:22:13 +0900

    Use nvm

--- zsh/envs/11-path-nodejs.zsh
+++ zsh/envs/11-path-nodejs.zsh
@@ -1,3 +1,8 @@
-# n (https://github.com/tj/n)
-export N_PREFIX="$HOME"/.n
-add_path "$N_PREFIX"/bin
+# nvm: Node Version Manager
+if [ -s /usr/local/opt/nvm/nvm.sh ]; then
+  # Homebrew
+  . /usr/local/opt/nvm/nvm.sh
+  . /usr/local/opt/nvm/etc/bash_completion.d/nvm
+elif [ -s /usr/share/nvm/init-nvm.sh ]; then
+  . /usr/share/nvm/init-nvm.sh
+fi

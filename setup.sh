144bd7b25adc25d27d82f141b8a66e53fa86fdef
Yusuke Kido <mail@yusuk.eki.do>
2023-02-26 13:44:49 +0900

    chshがユーザーのパスワードを聞いてくるのを回避するためsudoを使う

--- setup.sh
+++ setup.sh
@@ -101,7 +101,12 @@ echo '5. Setting login shell...'
 if [[ $SHELL =~ zsh$ ]]; then
     echo 'zsh is already your login shell'
 else
-    chsh -s $(grep -m 1 -F /zsh /etc/shells)
+    zsh_path=$(grep -m 1 -F /zsh /etc/shells)
+    if yesno 'Use sudo? (chsh may require user password without sudo)'; then
+        sudo chsh -s "$zsh_path" "$(whoami)"
+    else
+        chsh -s "$zsh_path"
+    fi
 
     if [ -x /opt/distrod/bin/distrod ]; then
         sudo /opt/distrod/bin/distrod enable

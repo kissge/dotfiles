378d42833c2628c96e794add8db70b34f856ad35
Yusuke Kido <mail@yusuk.eki.do>
2023-06-22 17:26:56 +0900

    Use asdf instead of nvm because nvm is too slow

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,6 +1,5 @@
 source "$ZDOTDIR"/envs/00-locale.zsh
 source "$ZDOTDIR"/envs/10-path.zsh
-source "$ZDOTDIR"/envs/11-path-nodejs.zsh
 source "$ZDOTDIR"/envs/30-editor.zsh
 source "$ZDOTDIR"/envs/40-coreutils.zsh
 source "$ZDOTDIR"/envs/41-bat.zsh
--- zsh/envs/11-path-nodejs.zsh
+++
@@ -1,8 +0,0 @@
-# nvm: Node Version Manager
-if [ -s /usr/local/opt/nvm/nvm.sh ]; then
-  # Homebrew
-  . /usr/local/opt/nvm/nvm.sh
-  . /usr/local/opt/nvm/etc/bash_completion.d/nvm
-elif [ -s /usr/share/nvm/init-nvm.sh ]; then
-  . /usr/share/nvm/init-nvm.sh
-fi

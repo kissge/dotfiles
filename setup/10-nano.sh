739bfa3816ece8421db3417cb7799a927d50dbb5
Yusuke Kido <mail@yusuk.eki.do>
2024-05-28 21:23:36 +0900

    Setup: Add nano

---
+++ setup/10-nano.sh
@@ -0,0 +1,3 @@
+if yesno 'Set up https://github.com/galenguyer/nano-syntax-highlighting for improved nano syntax highlighting?'; then
+    curl https://raw.githubusercontent.com/galenguyer/nano-syntax-highlighting/master/install.sh | sh
+fi

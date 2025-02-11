9af87c02a41d3b89f994496370ba320ef7d92b75
Yusuke Kido <mail@yusuk.eki.do>
2025-02-11 21:15:34 +0900

    Transition to LINE Messaging API

--- zsh/inits/32-hook-timer.zsh
+++ zsh/inits/32-hook-timer.zsh
@@ -9,7 +9,7 @@ function __hook_timer_precmd() {
         __hook_timer_start=
 
         if ((__hook_timer_duration > 300)); then
-            __hook_timer_report="Command $__hook_timer_command completed in $__hook_timer_duration seconds"
+            __hook_timer_report="\`$__hook_timer_command\` completed in $__hook_timer_duration seconds"
         else
             __hook_timer_report=
         fi
@@ -21,7 +21,7 @@ TMOUT=60
 function TRAPALRM() {
     # $TMOUT seconds elapsed since the prompt was displayed
     if [ -n "$__hook_timer_report" ]; then
-        line-me <<<"$__hook_timer_report" >/dev/null 2>&1
+        line-me <<< "$__hook_timer_report" > /dev/null 2>&1
         __hook_timer_report=
     fi
 }
